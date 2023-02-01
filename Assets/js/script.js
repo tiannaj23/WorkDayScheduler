c7b7b195cc2dc6c444b9fd1e0d213ba5ed6e72b6
tiannaj23 <tiannaj23@gmail.com>
2023-02-01 18:30:32 -0500

    editing readME

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,25 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'))
 
   // loop time blocks
   for (let i = 9; i < 17 ; i++) {
 
+    //adding AM to the time
     var time = 'AM';
     
+    //adding PM to the time if it's greater than 11
+    if (i > 11) {
+      time = 'PM'
+    }
+
+    //creating variables for each element
     var rowEL = $('<div>');
     var hourEl = $('<div>');
     var textArea = $('<textarea>');
     var buttonEl = $('<button>');
     var iconEl = $('<i>');
 
+    //adding each class
     rowEL.addClass('row time-block');
     hourEl.addClass('col-2 col-md-1 hour text-center py-3');
     textArea.addClass('col-8 col-md-10 description');
@@ -34,22 +38,14 @@ $(function () {
   function click(){
     console.log(this);
   }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 });
--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ## Description
 
-
+The Work Day Scheduler is used to create a schedule. Important events are added to a daily planner, so that time is managed effectively.
 
 
 ## Installation
 
 
 ## Usage
-
+To navigate the Work Day Scheduler website, you can scroll down and view the time blocks for that day. When you click into a time block, enter an event, then you will be able to save that event into local storage. 
 
 
 ## Credits
@@ -18,3 +18,5 @@
 ## Mock-Up
 
 The following screenshot shows the deployed website and the link will take you to live deployed website.
+
+![screen shot](./images/Mock-Up%20%20Large.jpeg)
